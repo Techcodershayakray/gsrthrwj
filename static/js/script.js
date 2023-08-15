bdde83f25a25a3b274f6a94820e4e49867936b3f
Shayak Ray <87775738+TechcoderShayak@users.noreply.github.com>
2023-08-15 23:41:03 +0530

    Delete static directory

--- static/js/script.js
+++
@@ -1,9 +0,0 @@
-// JavaScript for uploaded.html
-document.addEventListener("DOMContentLoaded", function () {
-    const resultItems = document.querySelectorAll(".result-item");
-    resultItems.forEach((item) => {
-        item.addEventListener("click", function () {
-            alert(`You clicked on: ${item.textContent}`);
-        });
-    });
-});
